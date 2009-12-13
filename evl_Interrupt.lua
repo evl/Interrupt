1b226a0991eec4c2f4b80327ee467450a9a11099
Evl <wow.evl@gmail.com>
2009-12-13 20:52:44 +0100

    Initial version complete.

--- evl_Interrupt.lua
+++ evl_Interrupt.lua
@@ -1,52 +1,6 @@
 evl_Interrupt = CreateFrame("Frame")
 evl_Interrupt.config = {
 	party = {"PARTY"},
-	raid = {"CHANNEL", "namrogue"}
+	raid = {"CHANNEL", "namrogue"},
+	solo = nil,
 }
-
-local interruptSpells = {
-	[1766] = "Kick",
-	[47528] = "Mind Freeze",
-	[6552] = "Pummel",
-	[72] = "Shield Bash",
-	[2139] = "Counterspell"
-}
-
-local onEvent = function(event, timestamp, eventType, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags, spellId, spellName, _, missType)
-	if bit.band(sourceFlags, COMBATLOG_FILTER_ME) == COMBATLOG_FILTER_ME then
-		if eventType == "SPELL_INTERRUPT" then
-	    local icon = CombatLog_String_GetIcon(destFlags)
-	    print("Interrupted", spellName, icon, destName)
-		--elseif eventType == "SPELL_CAST_SUCCESS" and interruptSpells[spellId] then
-		--	unit = getUnit(destGUID)
-	    	
-		--	if unit and not UnitCastingInfo(unit) then
-		--		print("Used", spellName, destName)
-		--	end
-		elseif eventType == "SPELL_MISSED" and interruptSpells[spellId] then
-	    local icon = CombatLog_String_GetIcon(destFlags)
-	    print("Missed", spellName, icon, destName)
-		end
-	end
-end
-
-evl_Interrupt:SetScript("OnEvent", onEvent)
-
--- Chat filter
-local interruptFilter = function(self, event, text, author, ...)
-	if find(text, incomingLinkPattern) then
-		text = gsub(text, "{CLINK:(%x+):([%d-]-:[%d-]-:[%d-]-:[%d-]-:[%d-]-:[%d-]-:[%d-]-:[%d-]-:[%d-]-):([^}]-)}", "|c%1|Hitem:%2|h[%3]|h|r")
-		text = gsub(text, "{CLINK:achievement:(%x+):(%-?%d-:%-?%x-:%-?%d-:%-?%d-:%-?%d-:%-?%d-:%-?%d-:%-?%d-:%-?%d-:%-?%d-):([^}]-)}", "|c%1|Hachievement:%2|h[%3]|h|r")
-		text = gsub(text, "{CLINK:enchant:(%x+):([%d-]-):([^}]-)}", "|c%1|Henchant:%2|h[%3]|h|r")
-		text = gsub(text, "{CLINK:glyph:(%x+):([%d-]-:[%d-]-):([^}]-)}", "|c%1|Hglyph:%2|h[%3]|h|r")
-		text = gsub(text, "{CLINK:item:(%x+):([%d-]-:[%d-]-:[%d-]-:[%d-]-:[%d-]-:[%d-]-:[%d-]-:[%d-]-:[%d-]-):([^}]-)}", "|c%1|Hitem:%2|h[%3]|h|r")
-		text = gsub(text, "{CLINK:quest:(%x+):([%d-]-):([%d-]-):([^}]-)}", "|c%1|Hquest:%2:%3|h[%4]|h|r")
-		text = gsub(text, "{CLINK:spell:(%x+):([%d-]-):([^}]-)}", "|c%1|Hspell:%2|h[%3]|h|r")
-		text = gsub(text, "{CLINK:talent:(%x+):([%d-]-:[%d-]-):([^}]-)}", "|c%1|Htalent:%2|h[%3]|h|r")
-		text = gsub(text, "{CLINK:trade:(%x+):(%-?%d-:%-?%d-:.*:.*):([^}]-)}", "|c%1|Htrade:%2|h[%3]|h|r")
-		
-		return false, text, author, ...
-	end
-end
-
-ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", incomingLinkFilter)
--- modules/main.lua
+++ modules/main.lua
@@ -27,30 +27,35 @@ local announce = function(message)
 	end
 end
 
-local onEvent = function(self, event, _, eventType, _, sourceName, sourceFlags, _, destName, destFlags, ...)
-	if bit.band(sourceFlags, COMBATLOG_FILTER_ME) == COMBATLOG_FILTER_ME then
+local bit_band = _G.bit.band
+
+local resolveUnit = function(guid)
+	return (UnitGUID("target") and "target") or (guid == UnitGUID("focus") and "focus") or (guid == UnitGUID("mouseover") and "mouseover")
+end
+
+local onEvent = function(self, event, _, eventType, _, sourceName, sourceFlags, destGUID, destName, destFlags, ...)
+	if bit_band(sourceFlags, COMBATLOG_FILTER_ME) == COMBATLOG_FILTER_ME then
 		if eventType == "SPELL_INTERRUPT" then
 			local _, spellName, _, _, destSpellName = ...
 	    local destIcon = CombatLog_String_GetIcon(destFlags)
+
 			announce(format("%sed %s%s's %s", spellName, destIcon, destName, destSpellName))
-	
-	    --print("Interrupted", spellName, icon, destName)
 		elseif eventType == "SPELL_CAST_SUCCESS" then
-			print("Cast success", ...)
 			local spellId, spellName = ...
-			--	unit = getUnit(destGUID)
+			local destIcon = CombatLog_String_GetIcon(destFlags)
 
 			if interruptSpells[spellId] then
-				--	if unit and not UnitCastingInfo(unit) then
-				print("Used", spellName, destName)
-				--	end
+				local unit = resolveUnit(destGUID)
+				
+				if not UnitCastingInfo(unit) then
+					announce(format("Used %s on %s%s", spellName, destIcon, destName))
+				end
 			end
 		elseif eventType == "SPELL_MISSED" and interruptSpells[spellId] then
 			local _, spellName, _, missType = ...
 	    local destIcon = CombatLog_String_GetIcon(destFlags)
+
 			announce(format("Missed %s on %s%s (%s)", spellName, destIcon, destName, missType))
-			
-			print("Missed", spellName, icon, destName)
 		end
 	end
 end
@@ -58,7 +63,9 @@ end
 evl_Interrupt:SetScript("OnEvent", onEvent)
 evl_Interrupt:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 
-local interruptFilter = function(self, event, text, author, ...)
+local find = _G.string.find
+
+local interruptMessageFilter = function(self, event, text, author, ...)
 	local ignore = true
 	
 	if find(text, "   $") then
@@ -82,4 +89,4 @@ local interruptFilter = function(self, event, text, author, ...)
 	end
 end
 
-ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", interruptMessageFilter)
+--ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", interruptMessageFilter)
